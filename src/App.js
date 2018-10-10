40f6a7ad850d2705fa0c0d6771eb9f71ca9b71f7
github username <github email address>
2018-10-10 15:59:04 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,49 @@ import React from 'react'
 import PokemonIndex from './components/PokemonIndex'
 import './App.css'
 
-const App = () => (
-  <div className="App">
-    <PokemonIndex />
-  </div>
-)
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      pokemon: [],
+      searchTerm: ''
+    }
+    fetch('http://localhost:3000/pokemon')
+    .then( data => data.json())
+    .then( response => {
+      this.setState({
+        pokemon: response
+      })
+    })
+  }
+  createPokemon = (pokeObj) => {
+
+    fetch('http://localhost:3000/pokemon',
+    {method: "POST",
+    headers: {
+      'accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(pokeObj)})
+    .then( data => data.json())
+    .then( response => this.setState({
+      pokemon: [
+        ...this.state.pokemon,
+        response
+      ]
+    }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <PokemonIndex pokemon={this.state.pokemon} createPokemon={this.createPokemon} />
+      </div>
+    )
+  }
+}
+
+
+
 
 export default App
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,41 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      src: props.data.sprites.front
+    }
+  }
+  handleClick = (event) => {
+    if (this.state.src === this.props.data.sprites.front) {
+      this.setState({
+        src: this.props.data.sprites.back
+      })
+    } else {
+      this.setState({
+        src: this.props.data.sprites.front
+      })
+    }
+  }
   render() {
+    const data = this.props.data
+    const hpStat = data.stats.find((obj) => {
+      return obj.name === "hp"
+    })
     return (
-      <Card>
+      <Card onClick={this.handleClick}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.state.src} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{data.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hpStat.value}
             </span>
           </div>
         </div>
