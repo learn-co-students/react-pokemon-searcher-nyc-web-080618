6459fd303e02ce87cb65b4e6a8cbe7fddc591e82
github username <github email address>
2018-10-13 23:40:49 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -4,9 +4,13 @@ import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
   render() {
+    const cards=this.props.pokemon.map((pokemon,idx)=>{
+      return <PokemonCard key={idx} {...pokemon} />
+      })
     return (
       <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
+        {cards}
       </Card.Group>
     )
   }
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,16 +13,25 @@ class PokemonForm extends React.Component {
     }
   }
 
+  handleChange=(e,{name,value})=>{
+    this.setState({[name]:value})
+  }
+
+  handleSubmit=()=>{
+    const{name,hp,frontUrl,backUrl}=this.state
+    this.props.handleSubmit(name,hp,frontUrl,backUrl)
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input onChange={this.handleChange} fluid label="Name" placeholder="Name" name="name" value={this.state.name} />
+            <Form.Input onChange={this.handleChange} fluid label="hp" placeholder="hp" name="hp" value={this.state.hp}/>
+            <Form.Input onChange={this.handleChange} fluid label="Front Image URL" placeholder="url" name="frontUrl" value={this.state.front}/>
+            <Form.Input onChange={this.handleChange} fluid label="Back Image URL" placeholder="url" name="backUrl" value={this.state.backUrl}/>
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
